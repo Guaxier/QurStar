36409a218e0cc8f3484c764de269ad13536bb33e
不会飞的柠檬 <113287234+Guaxier@users.noreply.github.com>
2024-05-29 14:44:25 +0800

    q礼物api测试代码

--- api8848/SystemGift/systemgift.php
+++ api8848/SystemGift/systemgift.php
@@ -1,65 +1,52 @@
 <?php
 
-function selectgitftype()
+//输出商品列表
+function selectGitfShop()
 {
-    $data = [
-        ["数据标识",
-        "物品分类列表",
-        "对象分类列表",
-        "精选数据",
+    $shopdata = [
+        [
+            "imgsrc" => 'baidu.com',//商品图片
+            "href" => "a.com",//商品详情地址
+            "text" => "内容",//商品简介
+            "title" => "标题",//商品标题
+            "like" => "88",//点赞数量
+            "cletime" => "2022/02/02 8:8:8"//发布日期
+            "imgsrc" => 'baidu.com',//用户头像地址
+            "text" => "内容", //用户昵称
+            "href" => "a.com"
         ],
-        ["数据内容",
-            [
-                [
-                    "href" => "baidu.com",
-                    "class" => "aaa",
-                    "text"=>"内容"
-                ],
-                [
-                    "href" => "baidu.com",
-                    "class" => "aaa",
-                    "text"=>"内容"
-                ],
-                [
-                    "href" => "baidu.com",
-                    "class" => "aaa",
-                    "text"=>"内容"
-                ],
-            ],
-
-            [
-                "class"=>"内容",
-                "href"=>"a.com"
-            ],
-            [
-                [
-                    "imgsrc" => 'baidu.com',//商品图片
-                    "href" => "a.com",//商品详情地址
-                    "text" => "内容",//商品简介
-                    "title" => "标题",//商品标题
-                    "like" => "88",//点赞数量
-                    "cletime" => "2022/02/02 8:8:8"//发布日期
-                ],
-                [
-                    "imgsrc" => 'baidu.com',//用户头像地址
-                    "text" => "内容", //用户昵称
-                    "href" => "a.com"
-                ]
-            ],
+        [
+            "imgsrc" => 'baidu.com',//商品图片
+            "href" => "a.com",//商品详情地址
+            "text" => "内容",//商品简介
+            "title" => "标题",//商品标题
+            "like" => "88",//点赞数量
+            "cletime" => "2022/02/02 8:8:8"//发布日期
+            "imgsrc" => 'baidu.com',//用户头像地址
+            "text" => "内容", //用户昵称
+            "href" => "a.com"
+        ],
+        [
+            "imgsrc" => 'baidu.com',//商品图片
+            "href" => "a.com",//商品详情地址
+            "text" => "内容",//商品简介
+            "title" => "标题",//商品标题
+            "like" => "88",//点赞数量
+            "cletime" => "2022/02/02 8:8:8"//发布日期
+            "imgsrc" => 'baidu.com',//用户头像地址
+            "text" => "内容", //用户昵称
+            "href" => "a.com"
         ]
-
-
-
-
-
-
-
-
-
-
     ];
         // 将数组转换为JSON格式
-        $jsonResult = json_encode($data, JSON_UNESCAPED_UNICODE); // 为了保持中文字符不变，使用JSON_UNESCAPED_UNICODE
+        $jsonResult = json_encode($shopdata, JSON_UNESCAPED_UNICODE); // 为了保持中文字符不变，使用JSON_UNESCAPED_UNICODE
         return $jsonResult;
 }
-
+//输出商品种类
+function selectGitfType()
+{
+    $shopType = ["男生","女生","爸爸","妈妈","孩子","老人","老师","客户","女生"];
+        // 将数组转换为JSON格式
+        $jsonResult = json_encode($shopType, JSON_UNESCAPED_UNICODE); // 为了保持中文字符不变，使用JSON_UNESCAPED_UNICODE
+        return $jsonResult;
+}
